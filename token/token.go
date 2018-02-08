bfae54a54a27d7c4514051d095193ac78924ada8
Daniel Esquivias <daniel.esquivias@gmail.com>
2018-02-07 19:43:39 -0800

    1.2 - Defining Our Tokens

---
+++ token/token.go
@@ -0,0 +1,75 @@
+package token
+
+// Type will be used as token.Type by other packages; avoid stutter by calling this Type and not TokenType.
+type Type string
+
+// Token data structure
+type Token struct {
+	Type    Type   // string;
+	Literal string // string; has the advantage of being easy to debug
+}
+
+// Define the possible Token.Type as constants
+const (
+	//
+	// Special Types
+	//
+
+	// ILLEGAL an illegal or unknown token type
+	ILLEGAL = "ILLEGAL"
+
+	// EOF is end of file
+	EOF = "EOF"
+
+	//
+	// Identifiers & Literals
+	//
+
+	// IDENT is an identifier type
+	IDENT = "IDENT"
+
+	// INT is an integer type
+	INT = "INT"
+
+	//
+	// Operators
+	//
+
+	// ASSIGN is an operator type
+	ASSIGN = "="
+
+	// PLUS is an operator type
+	PLUS = "+"
+
+	//
+	// Delimiters
+	//
+
+	// COMMA is a delimiter type
+	COMMA = ","
+
+	// SEMICOLON is a delimiter type
+	SEMICOLON = ";"
+
+	// LPAREN  is a delimiter type
+	LPAREN = "("
+
+	// RPAREN  is a delimiter type
+	RPAREN = ")"
+
+	// LBRACE  is a delimiter type
+	LBRACE = "{"
+
+	// RBRACE  is a delimiter type
+	RBRACE = "}"
+
+	//
+	// Keywords
+	//
+
+	// FUNCTION is a keyword type
+	FUNCTION = "FUNCTION"
+
+	// LET is a keyword type
+	LET = "LET"
+)
